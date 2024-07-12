79b0f892b70eb142c4e814aa990c9e2fa5bb05e7
Glitch (tinted-furtive-tangerine) <none>
2024-07-12 03:59:42 +0000

    👽👩‍👩‍👦 Checkpoint ./server.js:69073492/404

--- server.js
+++ server.js
@@ -2,11 +2,28 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt      = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  
+  bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
